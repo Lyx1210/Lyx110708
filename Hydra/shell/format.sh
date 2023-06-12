7c52a3d6ca72ae1ce0f01b6a86bd0649cf2f022a
Lyx1210 <131420748+Lyx1210@users.noreply.github.com>
2023-06-12 14:08:24 +0800

    Delete Hydra directory

--- Hydra/shell/format.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place hydra --exclude=__init__.py
-black hydra
-isort --recursive --apply hydra
--- Hydra/shell/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-#pytest -s --cov=hydra --cov-report=term-missing --cov-report=html  hydra/tests
-pytest --cov=hydra --cov-report=term-missing hydra/tests --cov-fail-under=95
